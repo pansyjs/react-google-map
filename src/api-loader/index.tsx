bd403ceee24f5692bd54de6e6c09db72d7c78c2b
wangxingkang <156148958@qq.com>
2021-12-05 20:15:54 +0800

    🎉 chore: project init

---
+++ src/api-loader/index.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+
+export interface APILoaderProps {}
+
+export const APILoader: React.FC<APILoaderProps> = ({}) => {
+  return null;
+};
